b03a0a5dc7b5f9d1eac101d2b1882ac244af9cd1
evgenkot <evgenkot@tuta.io>
2024-09-11 17:44:15 +0500

    py1-2 java1

---
+++ java/taks1/Main.java
@@ -0,0 +1,10 @@
+import ru.raschupkin1.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+        NewHelloWorld helloWorld = new NewHelloWorld();
+        helloWorld.sayHello();
+    }
+}
+
---
+++ java/taks1/ru/raschupkin1/NewHelloWorld.java
@@ -0,0 +1,8 @@
+package ru.raschupkin1;
+
+public class NewHelloWorld {
+    public void sayHello() {
+        System.out.println("Hello from NewHelloWorld class!");
+    }
+}
+
